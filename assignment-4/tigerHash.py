113aaa945c04a6d20237738f1a1527cb4ae4c05c
Max <maxmillan.ries@yahoo.com>
2020-10-16 14:19:23 +0200

    Finalized Tiger Hash Algorithm
    Fixed the a, b and c xoring calculation for each iteration.

--- assignment-4/tigerHash.py
+++ assignment-4/tigerHash.py
@@ -1,11 +1,16 @@
 import sys
 from sboxes import t1, t2, t3, t4
 
+
+# Inner Tiger Round
 def inner_round(block, a, b, c, m):
+
+    # Mask used to "support" unsigned long long
     mask = 0xFFFFFFFFFFFFFFFF
     c = c ^ block
     c = c & mask
 
+    # Used S-boxes found on the internet
     c_array = c.to_bytes(8, 'little')
     a -= t1[c_array[0]] ^ t2[c_array[2]] ^ t3[c_array[4]] ^ t4[c_array[6]]
     b += t4[c_array[1]] ^ t3[c_array[3]] ^ t2[c_array[5]] ^ t1[c_array[7]]
@@ -18,6 +23,7 @@ def inner_round(block, a, b, c, m):
     return a, b, c
 
 
+# Outer Tiger Round performed recursively.
 def outer_round(blocks, a, b, c, m, i=0):
     if i >= 8:
         return a, b, c
@@ -35,6 +41,7 @@ def outer_round(blocks, a, b, c, m, i=0):
         return outer_round(blocks, a, b, c, m, i + 1)
 
 
+# Implemented Key Scheduling Tiger function.
 def key_schedule(w):
     mask = 0xFFFFFFFFFFFFFFFF
     w[0] = (w[0] - (w[7] ^ 0xA5A5A5A5A5A5A5A5) & mask) & mask
@@ -55,31 +62,21 @@ def key_schedule(w):
     w[7] = (w[7] - (w[6] ^ 0x0123456789ABCDEF) & mask) & mask
 
 
+# Splits text into 64 byte chunks
 def split_text(text):
     return [text[i:i + 64] for i in range(0, len(text), 64)]
 
 
+# Pad original text to fit Tiger Hash requirements
 def pad(text):
-    text_length = len(text)
     original_length = len(text)
 
     # Pad with 0x01
     text += b'\x01'
-    text_length += 1
 
     # Number of zeros to pad
-    zero_count = 0
-    if text_length % 64 <= 56:
-        zero_count = abs(text_length % 64 - 56)
-    else:
-        while text_length % 64 != 56:
-            zero_count += 1
-            text_length += 1
-
-    # Pad with zeros
-    for i in range(zero_count):
+    while len(text) % 64 != 56:
         text += b'\x00'
-        text_length += 1
 
     weird_bit = (original_length * 8).to_bytes(8, 'little')
 
@@ -87,27 +84,36 @@ def pad(text):
     return text
 
 
+# Takes a text and output the 3 respective components used for hashing
 def hash(text):
     a = 0x0123456789ABCDEF
     b = 0xFEDCBA9876543210
     c = 0xF096A5B4C3B2E187
     mask = 0xFFFFFFFFFFFFFFFF
 
-    a_original = a
-    b_original = b
-    c_original = c
-
     blocks = split_text(pad(text))
+
     for block in blocks:
+
+        # Stores previous a, b and c values for final round operation
+        a_prev = a
+        b_prev = b
+        c_prev = c
+
+        # Splits 64 byte chunk into 8 blocks of 8 bytes
         split = [block[i:i + 8] for i in range(0, len(block), 8)]
+        # Transforms split into it's number representation
         w = [int.from_bytes(b, byteorder='little') for b in split]
+
         a, b, c = outer_round(w, a, b, c, 5)
         key_schedule(w)
         c, a, b = outer_round(w, c, a, b, 7)
         key_schedule(w)
         b, c, a = outer_round(w, b, c, a, 9)
 
-    return a ^ a_original, (b - b_original) & mask, (c + c_original) & mask
+        a, b, c = a ^ a_prev, (b - b_prev) & mask, (c + c_prev) & mask
+
+    return a, b, c
 
 
 if __name__ == '__main__':
